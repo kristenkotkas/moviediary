397d95f4302f1b6aeae7fec350bc136be0d0ebf4
Kristen Kotkas <kristen.kotkas@gmail.com>
2017-03-15 13:38:31 +0200

    wishlist timeout

--- src/main/resources/static/js/pages/wishlist.js
+++ src/main/resources/static/js/pages/wishlist.js
@@ -25,13 +25,16 @@ function addTableHead(lang) {
 }
 
 function addTableData(data, lang) {
+    var timeout = 100;
     $.each(data, function (i) {
-        $("#wishlist-table").append(
-            $.parseHTML('<tr>' +
-                '<td class="table-row">' + data[i]['Title'] + '</td>' +
-                //'<td class="center">' + getMonth(data[i]['Time'], lang) + '</td>' +
-                '<td class="center">' + data[i]['Time'] + '</td>' +
-                '</tr>')
-        );
+        setTimeout(function () {
+            $("#wishlist-table").append(
+                $.parseHTML('<tr>' +
+                    '<td class="table-row">' + data[i]['Title'] + '</td>' +
+                    //'<td class="center">' + getMonth(data[i]['Time'], lang) + '</td>' +
+                    '<td class="center">' + data[i]['Time'] + '</td>' +
+                    '</tr>')
+            );
+        }, timeout += 25);
     });
 }
