bbef2605835275fbe7dcad0989008cf5a1eb8bde
kristjanhk <kristjankyngas@outlook.com>
2017-04-01 16:38:21 +0300

    natuke ilusam user leht

--- src/main/resources/static/js/pages/user.js
+++ src/main/resources/static/js/pages/user.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     $(".sidebar-collapse").sideNav(); //sidebar initialization
+    initMap();
 });
 
 var eventbus = new EventBus("/eventbus");
@@ -20,23 +21,21 @@ eventbus.onopen = function () {
     $("#SendMessage").click(function () {
         sendMessage();
     });
+};
 
-    var initMap = function () {
-        var liivi2 = {lat: 58.378367, lng: 26.714695};
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 18,
-            center: liivi2
-        });
-        var marker = new google.maps.Marker({
-            position: liivi2,
-            map: map
-        });
-    }
-
-    //$("#map"). = initMap();
-    $(document).ready(function () {
-        initMap();
-    })
+var initMap = function () {
+    var liivi2 = {
+        lat: 58.378367,
+        lng: 26.714695
+    };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 18,
+        center: liivi2
+    });
+    var marker = new google.maps.Marker({
+        position: liivi2,
+        map: map
+    });
 };
 
 $.ajax({
