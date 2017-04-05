d6740b394b73bc1e86a2ec94e99e202e12ead888
kristenkotkas <kristen.kotkas@gmail.com>
2017-04-06 00:04:17 +0300

    statistika search tools layout uuendus

--- src/main/resources/static/js/pages/stats.js
+++ src/main/resources/static/js/pages/stats.js
@@ -115,7 +115,10 @@ var yearsChartSmall = new Chart(yearsChartSmallCtx, {
 var timeChart = new Chart(timeChartCtx, {
     type : 'bar',
     data: {
-        labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
+        labels: [
+            '00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11',
+            '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'
+        ],
         datasets: [
             {
                 data: [],
@@ -129,7 +132,10 @@ var timeChart = new Chart(timeChartCtx, {
 var timeChartSmall = new Chart(timeChartSmallCtx, {
     type : 'horizontalBar',
     data: {
-        labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
+        labels: [
+            '00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11',
+            '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'
+        ],
         datasets: [
             {
                 data: [],
