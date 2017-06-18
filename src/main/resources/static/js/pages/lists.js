2971f65d95f21d5fd244b2aac7e18728bbf16f9f
kristenkotkas <kristen.kotkas@gmail.com>
2017-06-18 22:23:26 +0300

    listide lisamise modal

--- src/main/resources/static/js/pages/lists.js
+++ src/main/resources/static/js/pages/lists.js
@@ -1,4 +1,36 @@
 $("#navbar-lists").addClass('navbar-text-active');
 $(document).ready(function () {
     $(".sidebar-collapse").sideNav(); //sidebar initialization
+    $('.modal').modal({
+        complete: modalClose
+    }); //movies modal initialization
 });
+
+var eventbus = new EventBus("/eventbus");
+var inputNewListName = $('#createListName');
+var newListModal = $('#modalList');
+var listsCollapsable = $('#lists-coll');
+
+eventbus.onopen = function () {
+    var lang;
+    eventbus.send("translations", getCookie("lang"), function (error, reply) {
+        lang = reply.body;
+        listsCollapsable.collapsible('open', 0);
+    });
+};
+
+function modalClose() {
+    console.log('modal closed');
+    inputNewListName.val('');
+}
+
+function createNewList() {
+    if (inputNewListName.val().length > 0) {
+        console.log('new list created');
+        newListModal.modal('close');
+    }
+}
+
+function fillLists(data) {
+
+}
