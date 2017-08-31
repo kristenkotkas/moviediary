2821887fe360f5a995a176a5087a224a2b3a84a7
kristjanhk <kristjankyngas@outlook.com>
2017-08-31 23:41:35 +0300

    ui todos

---
+++ frontend/ui/src/main/java/ui/UiVerticle.js
@@ -0,0 +1,5 @@
+const Router = require("vertx-web-js/router");
+const StaticHandler = require("vertx-web-js/static_handler");
+
+//todo vertx module in javascript
+//todo use react itself to server side render first page into string
