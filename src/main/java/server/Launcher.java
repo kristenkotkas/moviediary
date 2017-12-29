dc8d35425fd4d4df3fcf24576f2bff93c986b427
Kristjan <kristjankyngas@outlook.com>
2017-12-29 18:34:57 +0200

    Improved logback logging

--- src/main/java/server/Launcher.java
+++ src/main/java/server/Launcher.java
@@ -3,11 +3,8 @@ package server;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.dns.AddressResolverOptions;
-import io.vertx.core.logging.Logger;
-import io.vertx.core.logging.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import server.verticle.ServerVerticle;
-
-import java.io.IOException;
 
 import static io.vertx.rxjava.core.RxHelper.deployVerticle;
 import static io.vertx.rxjava.core.Vertx.vertx;
@@ -18,15 +15,15 @@ import static server.util.FileUtils.getConfig;
  * Launches the server.
  * Forces DNS resolver to use Google's DNS servers.
  */
+@Slf4j
 public class Launcher {
-    private static final Logger LOG = LoggerFactory.getLogger(Launcher.class);
 
-    public static void main(String[] args) throws IOException {
-        setLoggingToSLF4J();
-        deployVerticle(vertx(new VertxOptions().setAddressResolverOptions(new AddressResolverOptions()
-                .addServer("8.8.8.8")
-                .addServer("8.8.4.4"))), new ServerVerticle(), new DeploymentOptions()
-                .setConfig(getConfig(args)))
-                .subscribe(ar -> LOG.info("Server up!"), LOG::error);
-    }
+  public static void main(String[] args) {
+    setLoggingToSLF4J();
+    deployVerticle(vertx(new VertxOptions().setAddressResolverOptions(new AddressResolverOptions()
+        .addServer("8.8.8.8")
+        .addServer("8.8.4.4"))), new ServerVerticle(), new DeploymentOptions()
+        .setConfig(getConfig(args)))
+        .subscribe(ar -> log.info("Server up!"), thr -> log.error("Failed to start server", thr));
+  }
 }
