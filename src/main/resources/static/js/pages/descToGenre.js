8ad1c167757abd469282582a3dacc60a6a50b202
kristenkotkas <kristen.kotkas@gmail.com>
2018-01-07 15:33:06 +0200

    Description to Genre frontend

---
+++ src/main/resources/static/js/pages/descToGenre.js
@@ -0,0 +1,45 @@
+console.log("hello");
+
+const getApiUrl = function () {
+  const url = window.location.origin + '/';
+  const localUrl = 'http://localhost:8081/';
+  return url.indexOf('localhost') !== -1 ? localUrl : url;
+};
+//https://movies.kyngas.eu/'
+console.log('API', getApiUrl());
+
+const suitableGenres = $("#suitable-genres");
+const getGenres = $("#get-genres");
+const inputArea = $("#plot-input-area");
+
+getGenres.click(function () {
+  loadGenres(inputArea.val());
+});
+
+function loadGenres(plot) {
+  console.log("get genres pressed");
+  console.log("PLOT", plot);
+
+  $.ajax({
+    type: "POST",
+    url: getApiUrl() + "public/api/v1/genres",
+    data: JSON.stringify({ "description": plot}),
+    success: function (res) {
+      fillGenres(res);
+    }
+  });
+}
+
+function fillGenres(result) {
+  console.log("RESULT", result);
+  result = $.parseJSON(result);
+  suitableGenres.empty().append(
+      $.parseHTML(
+          '<ul>' +
+          '<li><span class="descToGenreItem"><i class="fa fa-chevron-right" aria-hidden="true"></i> ' + result['best'] +'</span></li>' +
+          '<li><span class="descToGenreItem"><i class="fa fa-chevron-right" aria-hidden="true"></i> ' + result['second'] +'</span></li>' +
+          '<li><span class="descToGenreItem"><i class="fa fa-chevron-right" aria-hidden="true"></i> ' + result['third'] +'</span></li>' +
+          '</ul>'
+      )
+  );
+}
